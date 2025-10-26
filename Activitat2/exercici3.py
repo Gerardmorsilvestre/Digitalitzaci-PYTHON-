77292e5935fbdb1da8eb86dd42483435936a00fa
Gerardmorsilvestre <gerardmorsilvestre@gmail.com>
2025-10-26 18:54:37 +0100

    g

---
+++ Activitat2/exercici3.py
@@ -0,0 +1,3 @@
+input()
+nom = input("Com et dius? ")
+print("Benvingut" + nom + " a Barcelona")
---
+++ Activitat2/exercici4.py
@@ -0,0 +1,5 @@
+edat = int(input("Quina edat tens? "))
+if (edat >= 18):
+    print("Ets major de edat")
+else:
+    print("No ets major de edat")
