3cd4ad629407088f6f5df57952ed1a85a1db28f8
Gerardmorsilvestre <gerardmorsilvestre@gmail.com>
2025-12-01 14:50:23 +0100

    gf

--- Activitat 3.1/servidor.py
+++ Activitat 3.1/servidor.py
@@ -7,6 +7,16 @@ app = Flask(__name__)
 def hello():
     return "Hello World!!!"
 
+@app.route('/suma', method = ['GET'])
+def suma():
+    numero1 = request.args.get('a')
+    print(numero1)
+    numero2 = request.args.get('b')
+    print(numero2)
+    suma = int(numero1) + int(numero2)
+    
+    return "la suma de " +numero1, "+" +numero2 + "es = ", str(suma)
+
 if __name__ == '__main__':
     app.run(debug=True)
 
